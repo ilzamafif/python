1a13fc3d9fd718184f2f2e2fe472625e7e42a7fa
Ilzam afif <ilzamafif27@gmail.com>
2022-01-08 14:02:49 +0700

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -14,36 +14,91 @@ from urllib.parse import urlparse
 
 
 class Blockchain(object):
-    diffictuly_target = "0000"
+    difficulty_target = "0000"
 
-    def has_block(self, block):
+    def hash_block(self, block):
         block_encoded = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_encoded).hexdigest()
 
     def __init__(self):
+        self.nodes = set()
+
         self.chain = []
 
         self.current_transactions = []
 
-        genesis_hash = self.has_block("genesis_block")
+        genesis_hash = self.hash_block("genesis_block")
 
         self.append_block(
             hash_of_previous_block=genesis_hash,
             nonce=self.proof_of_work(0, genesis_hash, [])
         )
 
+    def add_node(self, address):
+        parse_url = urlparse(address)
+        self.nodes.add(parse_url.netloc)
+        print(parse_url.netloc)
+
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+
+            if block['hash_of_previous_block'] != self.hash_block(last_block):
+                return False
+
+            if not self.valid_proof(
+                    current_index,
+                    block['hash_of_previous_block'],
+                    block['transaction'],
+                    block['nonce']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def update_blockchain(self):
+        neighbours = self.nodes
+        new_chain = None
+
+        max_length = len(self.chain)
+
+        for node in neighbours:
+            response = requests.get(f'http://{node}/blockchain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+                if new_chain:
+                    self.chain = new_chain
+                    return True
+
+        return False
+
     def proof_of_work(self, index, hash_of_previous_block, transactions):
         nonce = 0
+
         while self.valid_proof(index, hash_of_previous_block, transactions, nonce) is False:
             nonce += 1
         return nonce
 
     def valid_proof(self, index, hash_of_previous_block, transactions, nonce):
         content = f'{index}{hash_of_previous_block}{transactions}{nonce}'.encode()
-        content_hash = hashlib.sha256(content).hexdigest()
-        return content_hash[:len(self.diffictuly_target)] == self.diffictuly_target
 
-    def append_block(self, hash_of_previous_block, nonce):
+        content_hash = hashlib.sha256(content).hexdigest()
+
+        return content_hash[:len(self.difficulty_target)] == self.difficulty_target
+
+    def append_block(self, nonce, hash_of_previous_block):
         block = {
             'index': len(self.chain),
             'timestamp': time(),
@@ -57,11 +112,11 @@ class Blockchain(object):
         self.chain.append(block)
         return block
 
-    def add_transaction(self, sender, repicient, amount):
+    def add_transaction(self, sender, recipient, amount):
         self.current_transactions.append({
             'amount': amount,
-            'sender': sender,
-            'repicient': repicient,
+            'recipient': recipient,
+            'sender': sender
         })
         return self.last_block['index'] + 1
 
@@ -79,7 +134,7 @@ blockchain = Blockchain()
 # routes
 
 
-@ app.route('/blockchain', methods=['GET'])
+@app.route('/blockchain', methods=['GET'])
 def full_chain():
     response = {
         'chain': blockchain.chain,
@@ -92,45 +147,83 @@ def full_chain():
 @app.route('/mine', methods=['GET'])
 def mine_block():
     blockchain.add_transaction(
-        sender=0,
-        repicient=node_identifier,
+        sender="0",
+        recipient=node_identifier,
         amount=1
     )
 
     last_block_hash = blockchain.hash_block(blockchain.last_block)
+
     index = len(blockchain.chain)
     nonce = blockchain.proof_of_work(
         index, last_block_hash, blockchain.current_transactions)
 
     block = blockchain.append_block(nonce, last_block_hash)
     response = {
-        'messege': "block baru telah di {mined}",
+        'message': "Block baru telah ditambahkan (mined)",
         'index': block['index'],
         'hash_of_previous_block': block['hash_of_previous_block'],
         'nonce': block['nonce'],
-        'transaction': block['transaction'],
+        'transaction': block['transaction']
     }
 
     return jsonify(response), 200
 
 
 @app.route('/transactions/new', methods=['POST'])
-def new_trasaction():
-    values = requests.get_json()
+def new_transaction():
+    values = request.get_json()
 
-    required_files = ['sender', 'repicient', 'amount']
+    required_fields = ['sender', 'recipient', 'amount']
     if not all(k in values for k in required_fields):
-        return "missing fields"
+        return ('Missing fields', 400)
 
-    index = blockchain.add_trasactions[
+    index = blockchain.add_transaction(
         values['sender'],
-        values['repicient'],
+        values['recipient'],
         values['amount']
-    ]
+    )
 
-    response = {'messege': f'transaksi akan ditambahkan ke blok {index}'}
-    return (jsonify(response), 200)
+    response = {'message': f'Transaksi akan ditambahkan ke blok {index}'}
+    return (jsonify(response), 201)
+
+
+@app.route('/nodes/add_nodes', methods=['POST'])
+def add_nodes():
+    values = request.get_json()
+    nodes = values.get('nodes')
+
+    if nodes is None:
+        return "Error, missing node(s) info", 400
+
+    for node in nodes:
+        blockchain.add_node(node)
+
+    response = {
+        'message': 'Node baru telah di tambahkan',
+        'nodes': list(blockchain.nodes)
+    }
+
+    return jsonify(response), 200
+
+
+@app.route('/nodes/sync', methods=['GET'])
+def sync():
+    updated = blockchain.update_blockchain()
+    if updated:
+        response = {
+            'message': 'Blockchain telah diupdtae dengan data terbaru',
+            'blockchain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Blockchain sudah menggunakan data paling baru',
+            'blockchain': blockchain.chain
+        }
+
+    return jsonify(response), 200
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(sys.argv[1]))
+
